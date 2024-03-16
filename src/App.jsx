9a60718b32e315b17c1af3bbab5cadf9cc88c7e1
TallTall22 <syuns1205@gmail.com>
2024-03-16 20:28:13 +0800

    add solutions, handlekeyup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import database from "./data/database.json"
+import Wordle from "./components/Wordle";
+
+function App() {
+  const [solution,setSolution]=useState(null)
+
+  useEffect(()=>{
+    const data=database.solutions
+    const randomsolution=data[Math.floor(Math.random()*data.length)]
+    setSolution(randomsolution.word)
+  },[setSolution])
+  return (
+    <div className="App">
+      <h1>Wordle (Lingo)</h1>
+      {solution&&<Wordle solution={solution}/>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useWordle.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+const useWordle=(solution)=>{
+  const [turn,setTurn]=useState(0)
+  const [currentGuess,setCurrentGuess]=useState('')
+  const [guesses,setGuesses]=useState([])
+  const [history,useHistory]=useState([])
+  const [isCorrect,useIsCorrect]=useState(false)
+  const formatGuess=()=>{
+    
+
+  }
+
+  const addNewGuess=()=>{
+
+  }
+
+  const handleKeyup=({key})=>{
+
+    if(key==='Backspace'){
+      setCurrentGuess((prev)=>{
+        return prev.slice(0,-1)
+      })
+    }
+
+    if(/^[A-Za-z]$/.test(key)){
+      if(currentGuess.length<5){
+        setCurrentGuess((prev)=>{
+          return prev+key
+        })
+      }
+    }
+  }
+
+  return {turn,currentGuess,guesses,isCorrect,handleKeyup}
+}
+
+export default useWordle
