e6dae47edacf0b8b743982d35b303e538de92a82
TallTall22 <syuns1205@gmail.com>
2024-03-16 22:49:04 +0800

    add guess logic

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,24 +3,74 @@ import { useState } from "react"
 const useWordle=(solution)=>{
   const [turn,setTurn]=useState(0)
   const [currentGuess,setCurrentGuess]=useState('')
-  const [guesses,setGuesses]=useState([])
-  const [history,useHistory]=useState([])
-  const [isCorrect,useIsCorrect]=useState(false)
+  const [guesses,setGuesses]=useState([...Array(6)])
+  const [history,setHistory]=useState([])
+  const [isCorrect,setIsCorrect]=useState(false)
   const formatGuess=()=>{
-    
+    let solutionArray=[...solution]
+    let formattedGuess=[...currentGuess].map((l)=>{
+      return {key:l,color:'grey'}
+    })
 
+    formattedGuess.forEach((l,i)=>{
+      if(solutionArray[i]===l.key){
+        formattedGuess[i].color='green'
+        solutionArray[i]=null
+      }
+    })
+
+    formattedGuess.forEach((l,i)=>{
+      if(solutionArray.includes(l.key)&&l.color!=='green'){
+        formattedGuess[i].color='yellow'
+        solutionArray[i]=null
+        solutionArray[solutionArray.indexOf(l.key)]=null
+      }
+    })
+
+    return formattedGuess
   }
 
-  const addNewGuess=()=>{
+  const addNewGuess=(formattedGuess)=>{
+    if(currentGuess===solution){
+      setIsCorrect(true)
+    }
+    setGuesses((prevGuesses)=>{
+      let newGuesses=[...prevGuesses]
+      newGuesses[turn]=formattedGuess
+      return newGuesses
+    })
 
+    setHistory((prevHistory)=>{
+      return [...prevHistory,currentGuess]
+    })
+    setTurn((prevTurn)=>{
+      return prevTurn+1
+    })
+    setCurrentGuess('')
   }
 
   const handleKeyup=({key})=>{
-
+    if(key==='Enter'){
+      if(turn>5){
+        console.log('you used all your guesses')
+        return
+      }
+      if(history.includes(currentGuess)){
+        console.log('you already tried that word')
+        return
+      }
+      if(currentGuess.length!==5){
+        console.log('word must be 5 chars long')
+        return
+      }
+      const formatted=formatGuess()
+      addNewGuess(formatted)
+    }
     if(key==='Backspace'){
       setCurrentGuess((prev)=>{
         return prev.slice(0,-1)
       })
+      return
     }
 
     if(/^[A-Za-z]$/.test(key)){
